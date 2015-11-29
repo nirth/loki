04ba9a64c58178c81a73f8d92ba5b76f6cee5b30
David Sergey <nirth.furzahad@gmail.com>
2015-11-29 07:30:43 +0000

    Project setup

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import babel from 'gulp-babel';
 import eslint from 'gulp-eslint';
 import mocha from 'gulp-mocha';
+import bump from 'gulp-bump';
 
 const SRC = 'src';
 const SPECS = 'specs';
@@ -19,6 +20,7 @@ gulp.task('copyStatics', () => gulp
 gulp.task('bdd', () => gulp
   .src(PATHS)
   .pipe(mocha({ reporter: 'nyan'}))
+  .pipe(mocha({ reporter: 'mocha-junit-reporter'}))
 );
 
 gulp.task('lint', () => gulp
@@ -28,6 +30,13 @@ gulp.task('lint', () => gulp
   .pipe(eslint.failOnError())
 );
 
+gulp.task('version', () => gulp
+  .src('./package.json')
+  .pipe(bump({type:'patch'}))
+  .pipe(gulp.dest('./'))
+);
+
 gulp.task('test', ['lint', 'bdd']);
 gulp.task('production', ['test', 'copyStatics']);
+gulp.task('release', ['production', 'version']);
 gulp.task('default', ['test']);
